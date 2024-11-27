c6d7b93b81c1756383dbb5a21c8cf0659f32ef7b
luciacantos <luciacantos22@gmail.com>
2024-11-27 02:36:12 +0100

    configuración redes sociales

--- webempresa/Services/views.py
+++ webempresa/Services/views.py
@@ -4,4 +4,4 @@ from .models import Service
 # Create your views here.
 def services(request):
     services = Service.objects.all()
-    return render(request, 'services/services.html', {services: services})
+    return render(request, 'services/services.html', {'services': services})
