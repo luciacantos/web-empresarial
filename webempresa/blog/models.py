380d9429faee32ae914dc51e18404d15f5588bd3
luciacantos <luciacantos22@gmail.com>
2024-11-27 02:10:23 +0100

    creacion app social

--- webempresa/blog/models.py
+++ webempresa/blog/models.py
@@ -23,7 +23,7 @@ class Post(models.Model):
     content = models.TextField(verbose_name="Contenido")
     published = models.DateTimeField(default=now, verbose_name="Fecha de publicación")
     image = models.ImageField(upload_to="blog", null=True, blank=True, verbose_name="Imagen")
-    author = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name="Autor") 
+    author = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name="Autor")
     categories = models.ManyToManyField(Category, verbose_name="Categorías", related_name="get_posts")
     created = models.DateTimeField(auto_now_add=True, verbose_name="Fecha de creación")
     updated = models.DateTimeField(auto_now=True, verbose_name="Fecha de edición")
