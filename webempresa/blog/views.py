4f8c626eb07bf1fdc9bdace0159aed1e60e91e17
luciacantos <luciacantos22@gmail.com>
2024-11-27 15:31:10 +0100

    corrección blog

--- webempresa/blog/views.py
+++ webempresa/blog/views.py
@@ -1,13 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+
 from .models import Post, Category
 
 # Create your views here.
+
 def blog(request):
     posts = Post.objects.all()
-    return render(request, 'blog/blog.html', {'posts': posts})
+    return render(request, 'blog/blog.html', {'posts':posts})
 
 def category(request, category_id):
-    posts = Post.objects.filter(categories=category)
-    category = Category.objects.get(id=category_id)
-    return render(request, 'blog/category.html', {'category': category, 'posts': posts})
-
+    category = get_object_or_404(Category, id=category_id)
+    return render(request, "blog/category.html",
+{'category':category})
