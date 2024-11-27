34616e8a134476a96c9806205f0e684b3bc8965c
luciacantos <luciacantos22@gmail.com>
2024-11-27 13:19:30 +0100

    arreglos contact

--- webempresa/core/views.py
+++ webempresa/core/views.py
@@ -7,11 +7,7 @@ def home(request):
 def about(request):
     return render(request, 'core/about.html')
 
-
-
 def store(request):
     return render(request, 'core/store.html')
 
-def contact(request):
-    return render(request, 'core/contact.html')
 
