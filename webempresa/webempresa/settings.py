b524bf4852baa59fd4010b5ffe58884a00663659
luciacantos <luciacantos22@gmail.com>
2024-12-01 18:55:51 +0100

    vercel

--- webempresa/webempresa/settings.py
+++ webempresa/webempresa/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-dluufb*0b%@c*xrf+lhe_qu=arf3o0m=vqm*y2hyrjmiu7)@)s
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app', 'localhost','127.0.0.1']
 
 
 # Application definition
@@ -56,6 +56,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhitenoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'webempresa.urls'
@@ -78,7 +79,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'webempresa.wsgi.application'
+WSGI_APPLICATION = 'VercelDEploy.wsgi.app'
 
 
 # Database
@@ -126,7 +127,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
