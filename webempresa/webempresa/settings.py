abe4b7c5bbfe4b680fb704a64b73c5104e55a1dc
luciacantos <luciacantos22@gmail.com>
2024-12-01 19:23:10 +0100

    corrección

--- webempresa/webempresa/settings.py
+++ webempresa/webempresa/settings.py
@@ -79,7 +79,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'VercelDeploy.wsgi.app'
+WSGI_APPLICATION = 'webempresa.wsgi.app'
 
 
 # Database
--- webempresa/webempresa/urls.py
+++ webempresa/webempresa/urls.py
@@ -17,6 +17,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('', include('core.urls')),
@@ -28,7 +29,6 @@ urlpatterns = [
 ]
 
 if settings.DEBUG:
-    from django.conf.urls.static import static
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
